2d8945b2d59f4452636598102e569c57c62439b5
Attila Mészáros <a_meszaros@apple.com>
2025-07-11 08:23:52 +0200

    test improvement, license fix
    Signed-off-by: Attila Mészáros <a_meszaros@apple.com>

--- spark-operator/src/test/java/org/apache/spark/k8s/operator/utils/StatusRecorderTest.java
+++ spark-operator/src/test/java/org/apache/spark/k8s/operator/utils/StatusRecorderTest.java
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.apache.spark.k8s.operator.utils;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -14,6 +33,7 @@ import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.client.server.mock.EnableKubernetesMockClient;
 import io.fabric8.kubernetes.client.server.mock.KubernetesMockServer;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 
 import org.apache.spark.k8s.operator.SparkApplication;
@@ -39,10 +59,10 @@ class StatusRecorderTest {
 
   @Test
   void retriesFailedStatusPatches() {
-    var testResource = TestUtils.createMockApp(DEFAULT_NS);
-    testResource.getMetadata().setResourceVersion("1");
-    var updated = TestUtils.createMockApp(DEFAULT_NS);
-    updated.getMetadata().setResourceVersion("2");
+    var testResource = getSparkApplication("1");
+    var resourceV2 = getSparkApplication("2");
+    var resourceV3 = getSparkApplication("3");
+
     BaseContext<SparkApplication> context = mock(BaseContext.class);
     when(context.getResource()).thenReturn(testResource);
     when(context.getClient()).thenReturn(client);
@@ -53,7 +73,9 @@ class StatusRecorderTest {
             + testResource.getMetadata().getName()
             + "/status";
     server.expect().withPath(path).andReturn(500, null).once();
-    server.expect().withPath(path).andReturn(200, updated).once();
+    server.expect().withPath(path).andReturn(200, resourceV2).once();
+    // this should be not called, thus updated resource should have resourceVersion 2
+    server.expect().withPath(path).andReturn(200, resourceV3).once();
 
     statusRecorder.persistStatus(context, new ApplicationStatus());
 
@@ -62,5 +84,11 @@ class StatusRecorderTest {
             assertArg(a -> assertThat(a.getMetadata().getResourceVersion()).isEqualTo("2")),
             any(),
             any());
+  }
+
+  private static @NotNull SparkApplication getSparkApplication(String resourceVersion) {
+    var updated = TestUtils.createMockApp(DEFAULT_NS);
+    updated.getMetadata().setResourceVersion(resourceVersion);
+    return updated;
   }
 }
