1ed8ce27df1de189831b832203ef578f696ed697
Qi Tan <16416018+TQJADE@users.noreply.github.com>
2025-08-05 20:41:03 +0200

    [SPARK-53026] Fix deserialize error for spec with DriverServiceIngressSpec field
    ### What changes were proposed in this pull request?
    Add two annotations for DriverServiceIngressSpec constructor
    
    ### Why are the changes needed?
    Without the annotation, when applying the spec with DriverServiceIngressSpec, deserialization failed.
    
    ### Does this PR introduce _any_ user-facing change?
    no
    
    ### How was this patch tested?
    unit test
    
    ### Was this patch authored or co-authored using generative AI tooling?
    no
    
    Closes #292 from TQJADE/ingress-fix.
    
    Authored-by: Qi Tan <16416018+TQJADE@users.noreply.github.com>
    Signed-off-by: Peter Toth <p_toth@apple.com>

--- spark-operator-api/src/test/java/org/apache/spark/k8s/operator/spec/ApplicationSpecTest.java
+++ spark-operator-api/src/test/java/org/apache/spark/k8s/operator/spec/ApplicationSpecTest.java
@@ -22,6 +22,10 @@ package org.apache.spark.k8s.operator.spec;
 import static org.apache.spark.k8s.operator.spec.DeploymentMode.ClusterMode;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 
 class ApplicationSpecTest {
@@ -45,5 +49,24 @@ class ApplicationSpecTest {
     assertEquals(0, tolerations.instanceConfig.initExecutors);
     assertEquals(0, tolerations.instanceConfig.minExecutors);
     assertEquals(0, tolerations.instanceConfig.maxExecutors);
+  }
+
+  @Test
+  void testSpecWithDriverServiceIngressList() throws IOException {
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    InputStream inputStream =
+        getClass()
+            .getClassLoader()
+            .getResourceAsStream("spark-job-with-driver-service" + "-ingress.json");
+    ApplicationSpec spec = objectMapper.readValue(inputStream, ApplicationSpec.class);
+    assertNotNull(spec.getDriverServiceIngressList());
+    assertEquals(1, spec.getDriverServiceIngressList().size());
+
+    DriverServiceIngressSpec driverServiceIngress = spec.getDriverServiceIngressList().get(0);
+    assertNotNull(driverServiceIngress.getIngressMetadata());
+    assertNotNull(driverServiceIngress.getIngressSpec());
+    assertNotNull(driverServiceIngress.getServiceSpec());
+    assertNotNull(driverServiceIngress.getServiceMetadata());
   }
 }
