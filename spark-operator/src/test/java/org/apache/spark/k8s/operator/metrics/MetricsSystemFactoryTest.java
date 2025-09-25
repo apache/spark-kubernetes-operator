679954ae27a3a352ae44e1eb8a977e324703e395
Dongjoon Hyun <dongjoon@apache.org>
2025-09-24 23:56:34 -0700

    [SPARK-53712] Fix `spark-operator` to use `JUnit 5` instead of `JUnit 4`
    ### What changes were proposed in this pull request?
    
    This PR aims to fix `spark-operator` module to use `JUnit 5` correctly instead of the transitive `JUnit 4` dependency.
    
    ### Why are the changes needed?
    
    `Apache Spark K8s Operator` should use `JUnit 5` consistently.
    
    https://github.com/apache/spark-kubernetes-operator/blob/a0929a4023f7a0f9eba5765fdb47e17228d68a0e/gradle/libs.versions.toml#L26
    
    **BEFORE**
    
    ```
    $ gradle :spark-operator:dependencies | grep ' junit:junit' | wc -l
           3
    ```
    
    **AFTER**
    
    ```
    $ gradle :spark-operator:dependencies | grep ' junit:junit' | wc -l
           0
    ```
    
    ### Does this PR introduce _any_ user-facing change?
    
    No, this is a test code and dependency change.
    
    ### How was this patch tested?
    
    Pass the CIs.
    
    ### Was this patch authored or co-authored using generative AI tooling?
    
    No.
    
    Closes #344 from dongjoon-hyun/SPARK-53712.
    
    Authored-by: Dongjoon Hyun <dongjoon@apache.org>
    Signed-off-by: Dongjoon Hyun <dongjoon@apache.org>

--- spark-operator/src/test/java/org/apache/spark/k8s/operator/metrics/MetricsSystemFactoryTest.java
+++ spark-operator/src/test/java/org/apache/spark/k8s/operator/metrics/MetricsSystemFactoryTest.java
@@ -20,7 +20,7 @@
 package org.apache.spark.k8s.operator.metrics;
 
 import static org.apache.spark.k8s.operator.metrics.MetricsSystemFactory.parseSinkProperties;
-import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.Properties;
 
--- spark-operator/src/test/java/org/apache/spark/k8s/operator/metrics/source/KubernetesMetricsInterceptorTest.java
+++ spark-operator/src/test/java/org/apache/spark/k8s/operator/metrics/source/KubernetesMetricsInterceptorTest.java
@@ -19,7 +19,7 @@
 
 package org.apache.spark.k8s.operator.metrics.source;
 
-import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.Arrays;
 import java.util.Collections;
